0667a49954619475afd9e2a9715881fbeca55cf7
Andrii Sova <asovacloud@gmail.com>
2025-02-06 11:15:06 +0100

    Add an Opener handler

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+from itertools import count
+
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, CallbackQueryHandler, CommandHandler
 
 from gpt import *
@@ -82,6 +84,78 @@ async def message_button(update, context):
     answer = await chatgpt.send_question(prompt, user_chat_history)
     await my_message.edit_text(answer)
 
+async def profile(update, context):
+    dialog.mode = "profile"
+    msg = load_message("profile")
+    await send_photo(update, context, "profile")
+    await send_text(update, context, msg)
+
+    dialog.user.clear()
+    dialog.counter = 0
+    await send_text(update, context, "Скільки вам років?")
+
+
+async  def profile_dialog(update, context):
+    text = update.message.text
+    dialog.counter += 1
+
+    if dialog.counter == 1:
+        dialog.user["age"] = text
+        await  send_text(update, context, "Ким ви працюєте?")
+    if dialog.counter == 2:
+        dialog.user["occupation"] = text
+        await send_text(update, context, "У вас є хобі?")
+    if dialog.counter == 3:
+        dialog.user["hobby"] = text
+        await  send_text(update, context, "Що вам НЕ подобається в людях?")
+    if dialog.counter == 4:
+        dialog.user["annoys"] = text
+        await send_text(update, context, "Мета знайомства?")
+    if dialog.counter == 5:
+        dialog.user["goals"] = text
+        prompt = load_prompt("profile")
+        user_info = dialog_user_info_to_str(dialog.user)
+
+        my_message = await send_text(update, context, "ChatGPT 🧠 генерує ваш профіль. Зачекайте кілька секунд")
+        answer = await chatgpt.send_question(prompt, user_info)
+        await my_message.edit_text(answer)
+
+async def opener (update, context):
+    dialog.mode = "opener"
+    msg = load_message("opener")
+    await send_photo(update, context, "opener")
+    await send_text(update, context, msg)
+
+    dialog.user.clear()
+    dialog.counter = 0
+    await send_text(update, context, "Імʼя партнера?")
+
+async def opener_dialog(update, context):
+    text = update.message.text
+    dialog.counter += 1
+
+    if dialog.counter == 1:
+        dialog.user["name"] = text
+        await  send_text(update, context, "Скільки років партнеру?")
+    if dialog.counter == 2:
+        dialog.user["age"] = text
+        await send_text(update, context, "Оцініть зовнішність: 1-10 балів?")
+    if dialog.counter == 3:
+        dialog.user["handsome"] = text
+        await  send_text(update, context, "Ким вона працює?")
+    if dialog.counter == 4:
+        dialog.user["occupation"] = text
+        await send_text(update, context, "Мета знайомства?")
+    if dialog.counter == 5:
+        dialog.user["goals"] = text
+        prompt = load_prompt("opener")
+        user_info = dialog_user_info_to_str(dialog.user)
+
+        my_message = await send_text(update, context, "ChatGPT 🧠 генерує ваше повідомлення...")
+        answer = await chatgpt.send_question(prompt, user_info)
+        await my_message.edit_text(answer)
+
+
 async def hello(update, context):
     if dialog.mode == "gpt":
         await gpt_dialog(update, context)
@@ -89,10 +163,16 @@ async def hello(update, context):
         await date_dialog(update, context)
     if dialog.mode == "message":
         await message_dialog(update, context)
+    if dialog.mode == "profile":
+        await profile_dialog(update, context)
+    if dialog.mode == "opener":
+        await opener_dialog(update, context)
 
 dialog = Dialog()
 dialog.mode = None
 dialog.list = []
+dialog.user = {}
+dialog.counter = 0
 
 chatgpt = ChatGptService(token="")
 
@@ -101,6 +181,8 @@ app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("gpt", gpt))
 app.add_handler(CommandHandler("date", date))
 app.add_handler(CommandHandler("message", message))
+app.add_handler(CommandHandler("profile", profile))
+app.add_handler(CommandHandler("opener", opener))
 
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, hello))
 app.add_handler(CallbackQueryHandler(date_button, pattern="^date_.*"))
